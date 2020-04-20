8fb1e8d870ec75afd99793bf0787b5ab64b4d9a5
EC2 Default User <ec2-user@ip-172-31-26-7.us-east-2.compute.internal>
2020-04-20 17:23:23 +0000

    dep

---
+++ deploy.py
@@ -0,0 +1,12 @@
+import boto3
+PROJECT = "aws-cloudformation"
+cf = boto3.client('cloudformation')
+with open('template.yaml', 'r') as file:
+    template = file.read()
+
+
+response = cf.create_stack(
+    StackName = PROJECT,
+    TemplateBody = template
+)
+print(response)
